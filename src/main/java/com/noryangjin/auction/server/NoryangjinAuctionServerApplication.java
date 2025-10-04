34d729114027d620aba584ab16510f48f925cd5a
권장순 <jsoonworld@gmail.com>
2025-10-04 21:38:19 +0900

    feat(project): init

---
+++ src/main/java/com/noryangjin/auction/server/NoryangjinAuctionServerApplication.java
@@ -0,0 +1,13 @@
+package com.noryangjin.auction.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoryangjinAuctionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoryangjinAuctionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noryangjin/auction/server/NoryangjinAuctionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noryangjin.auction.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoryangjinAuctionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
